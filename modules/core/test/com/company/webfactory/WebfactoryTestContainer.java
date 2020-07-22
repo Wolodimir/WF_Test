e2f850cbedc41cb92d2ebfcccc91920ee5d1b6fa
Wolodimir <dvdnent666@gmail.com>
2020-07-22 16:30:47 +0400

    Init the project

---
+++ modules/core/test/com/company/webfactory/WebfactoryTestContainer.java
@@ -0,0 +1,58 @@
+package com.company.webfactory;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WebfactoryTestContainer extends TestContainer {
+
+    public WebfactoryTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/webfactory/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/webfactory/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends WebfactoryTestContainer {
+
+        public static final WebfactoryTestContainer.Common INSTANCE = new WebfactoryTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
